de69fd83fdb6ffae3846cda68b90262da002d997
Izan Jaén Rico <ijr3@alumnes.udl.cat>
2026-02-24 17:49:22 +0100

    v.0.2.0 Home Screen añadida

--- app/app/src/main/java/com/example/mambajet/ui/theme/Color.kt
+++ app/app/src/main/java/com/example/mambajet/ui/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val MambaGreen = Color(0xFF2DB300)      // Verde sólido para Light
+
+val MambaNeon = Color(0xFFC4FF00)       // Verde neón para Dark
+val JetBlack = Color(0xFF0D0D0D)
+
+val DarkGrey = Color(0xFF1A1A1A)
+
+val LightGrey = Color(0xFFF5F5F5)
+
+val WhiteText = Color(0xFFFFFFFF)
+
+val ErrorRed = Color(0xFFCF6679)
